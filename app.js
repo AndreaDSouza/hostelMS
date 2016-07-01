e4422c11d34f49bfee2695649bc2ea53aa8bd2d2
Paul <pauljoshyk96@gmail.com>
2016-07-01 18:51:15 +0530

    Tags are still inline

--- app.js
+++ app.js
@@ -2,14 +2,9 @@
 var hostelMS = angular.module("hostelMS",[]);
 
 hostelMS.controller('mainController',["$scope",function($scope){
-    $scope.applicants = applicants;
+    $scope.people = applicants;
 }]);
 
-hostelMS.directive("inputItem",function(){
-    return{
-        template:'<div class = "left" > {{value[0]}} </div> <input class = "right"type = "text" ng-model = "value[1]">'
-    }
-});
 
 var applicants = 
 {
