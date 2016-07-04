4a1fe1c5ed2de019648492a4ea41d10200b96b9a
Paul <pauljoshyk96@gmail.com>
2016-07-05 00:05:53 +0530

    created faulty post button and function

--- client/app-client.js
+++ client/app-client.js
@@ -1,7 +1,21 @@
 var hostelClient = angular.module("hostelClient",[]);
 
-hostelClient.controller('formCtrl',["$scope",function($scope){
+hostelClient.controller('formCtrl',["$scope","$http",function($scope,$http){
+
     $scope.appDetails =  appDetails;
+    $scope.postJSON =  function postJSON()
+    {
+        console.log("Clicked");
+        $http.post('../server/applicants.json',{ appDetails: $scope.appDetails})
+        .success(function (result)
+        {
+            console.log("Nailed it!");
+        })
+
+        .error(function (data,status)
+        {
+            console.log("Whoops!");
+        });
+    };
+    
 }]);
-
-
