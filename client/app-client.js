712ad162d4ba7119a682595b495b60323dc1fb7b
Paul <pauljoshyk96@gmail.com>
2016-07-06 22:44:15 +0530

    commit before merging

--- client/app-client.js
+++ client/app-client.js
@@ -1,27 +1,28 @@
 var hostelClient = angular.module("hostelClient",[]);
 
-hostelClient.controller('formCtrl',["$scope","$http",function($scope,$http){
+hostelClient.controller('formCtrl',["$scope","$http","$log",function($scope,$http,$log){
 
     $scope.appDetails =  appDetails;
     $scope.postJSON =  function postJSON()
     {
-        console.log("Clicked");
-        $http.post('../server/applicants.json',{ appDetails: $scope.appDetails})
+        $scope.appDetails.push({
+        $http.post('../server/applicants.json',{ appDetails:$scope.appDetails})
         .success(function (result)
         {
-            console.log("Nailed it!");
+            $log.info("Nailed it!");
         })
 
         .error(function (data,status)
         {
-            console.log("Whoops!");
+            $log.info(data,status);
+            $log.error("Whoops!");
         });
     }
     
     $scope.outputStuff = function outputStuff(){
     
         console.log("Please work!");
-        
+    
     }
     
 }]);
