79dbd1ea7e1a969f47a245600cfdc7e04ed48338
Paul <pauljoshyk96@gmail.com>
2016-09-09 19:39:05 +0530

    calendar has bugs

--- pages/attendance/js/controllers.js
+++ pages/attendance/js/controllers.js
@@ -1,36 +1,39 @@
-calApp.controller("calCtrl",function($scope,$dbServices){
-	$scope.cal=calendar; 
+calApp.controller("calCtrl", function($scope){
+	$scope.calendar={
+		months:[
+			{ name:"January",dayCount:31 },
+			{ name:"February",dayCount:28 },
+			{ name:"March",dayCount:31 },
+			{ name:"April",dayCount:30 },
+			{ name:"May",dayCount:31 },
+			{ name:"June",dayCount:30 },
+			{ name:"July",dayCount:31 },
+			{ name:"August",dayCount:31 },
+			{ name:"September",dayCount:30 },
+			{ name:"October",dayCount:31 },
+			{ name:"November",dayCount:30 },
+			{ name:"December",dayCount:31 }
+		]
+	};
+	
+	var day = function(){
+		
+		this.absentees=[];
+		this.events=[];
+		
+	};
+	
+	$scope.calendar.dayGen = function(month)
+	{
+		this.months[month].days=[]; 
+		for(var i=1; i<this.months[month].dayCount; i++)
+		{
+			this.months[month].days[i]= new day();
+		}
+	};
+	
+	var d = new Date();
+	calendar.dayGen(d.getMonth());
 	console.log(calendar);
 });
-var day = function(){
-	
-	this.absentees=[];
-	this.events=[];
 
-};
-
-var calendar = {
-	months:[
-		{ name:"January",dayCount:31 },
-		{ name:"February",dayCount:28 },
-		{ name:"March",dayCount:31 },
-		{ name:"April",dayCount:30 },
-		{ name:"May",dayCount:31 },
-		{ name:"June",dayCount:30 },
-		{ name:"July",dayCount:31 },
-		{ name:"August",dayCount:31 },
-		{ name:"September",dayCount:30 },
-		{ name:"October",dayCount:31 },
-		{ name:"November",dayCount:30 },
-		{ name:"December",dayCount:31 }
-	]
-};
-
-calendar.dayGen = function(month)
-{
-	this.months[month].days=[]; 
-	for(var i=1; i<this.months[month].dayCount; i++)
-	{
-		this.months[month].days[i]= new day();
-	}
-}
